fe52410f2252e70b12be7c4af3fc77f99760694f
Phani Mukkamala <phanimukkamala@phani-mac-2021.local>
2021-10-29 09:02:27 +0800

    added window.web3.currentProvider

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,6 +26,9 @@ class Home extends Component {
                 window.web3 = new Web3(window.ethereum);
                 await window.ethereum.enable();
             }
+            else if(window.web3) {
+                window.web3 = new Web3(window.web3.currentProvider);
+            }
         });
     }
 
