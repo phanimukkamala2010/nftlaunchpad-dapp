ed597dff96fa153842846505af758169f69cc20b
EC2 Default User <ec2-user@ip-172-31-26-211.ap-southeast-1.compute.internal>
2022-02-05 05:36:43 +0000

    CryptoBabes

--- src/contracts/CryptoBabes.sol
+++ src/contracts/CryptoBabes.sol
@@ -6,31 +6,27 @@ import "./FullERC721.sol";
 
 contract CryptoBabes is ERC721Enumerable, Ownable {
 
-    uint256 public constant _maxSupply = 1000;
+    uint256 public _maxSupply = 1000;
     uint256 public _price = 0.05 ether;
     bool public _paused = false;
     
     string public constant _desc = "CryptoBabes is a unique collection of babes on the beach - dedicated to all the women who make our lives better everyday";
     string public constant _babeURI = "http://nft-launchpad.io/cryptobabes/cryptobabe-";
 
-    mapping (uint256 => string) private _tokenId2image;
-
     constructor() ERC721("CryptoBabes", "CRYPTOBABE") Ownable() {
 	}
 
-    function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "NFTLP #', Strings.toString(tokenId), 
-                            '", "description": "', _desc, '", "image": "data:image/svg+xml;base64,', 
-                            Base64.encode(bytes(_tokenId2image[tokenId])), '"}'))));
-        string memory output = string(abi.encodePacked('data:application/json;base64,', json));
-        return output;
+    function _baseURI() override internal view virtual returns (string memory) {
+        return _babeURI;
     }
     function mint(uint256 tokenId) external payable {
         require(!_paused, "Sale paused");
         require(tokenId > 0 && totalSupply() <= _maxSupply, "Exceeds maximum supply");
         require(msg.value >= _price, "Ether sent is not correct");
-        _tokenId2image[tokenId] = string(abi.encodePacked(_babeURI, tokenId));
         _safeMint(msg.sender, tokenId);
+    }
+    function setMaxSupply(uint256 newVal) external onlyOwner {
+        _maxSupply = newVal;
     }
     function setPrice(uint256 _newPrice) external onlyOwner {
         _price = _newPrice;
